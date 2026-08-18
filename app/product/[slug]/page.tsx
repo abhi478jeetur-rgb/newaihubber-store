64ea842443d3015f9a42e49d141106ca106b18bd
abhi478jeetur-rgb <abhi478jeetur@gmail.com>
2026-08-18 16:58:28 +0530

    Set 50 Micro SaaS eBook sales page to exact saasideas.in visual match design system

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { notFound } from 'next/navigation';
 import { Metadata } from 'next';
 import { getProductBySlug, getAllProducts } from '@/lib/products';
+import { SaasIdeasSalesPage } from '@/components/product/SaasIdeasSalesPage';
 import { ProductLanding } from '@/components/product/ProductLanding';
-import { EbookSalesPage } from '@/components/product/EbookSalesPage';
 import {
   generateProductJsonLd,
   generateBreadcrumbJsonLd,
@@ -86,7 +86,7 @@ export default async function ProductDetailPage({ params }: ProductPageProps) {
       )}
 
       {is50SaasEbook ? (
-        <EbookSalesPage product={product} />
+        <SaasIdeasSalesPage product={product} />
       ) : (
         <ProductLanding product={product} />
       )}
